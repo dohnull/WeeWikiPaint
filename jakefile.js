ca51a41cef18ca2df7d9041407787d978b367e6c
dohnull <dohnull@users.noreply.github.com>
2014-05-08 12:29:52 -0700

    Updated static code analysis to find all js files

--- jakefile.js
+++ jakefile.js
@@ -1,10 +1,37 @@
+/*global desc, task, jake, fail, complete */
+
+"use strict";
 task("default", ["Lint"]);
 
-desc("Lint everything");
-task("Lint", [], function() {
-    var lint = require("./build/lint/lint_runner.js");
-    lint.validateFile("jakefile.js", {}, {});
+function nodeListOptions() {
+    return {
+        bitwise: true,
+        curly: false,
+        eqeqeq: true,
+        forin: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        noempty: true,
+        nonew: true,
+        regexp: true,
+        undef: true,
+        strict: true,
+        trailing: true,
+        node: true
+    };
+}
 
+desc("Lint everything");
+task("Lint", [], function () {
+    var lint = require("./build/lint/lint_runner.js");
+
+    var files = new jake.FileList();
+    files.include("**/*.js");
+    files.exclude("node_modules");
+    files.exclude("build");
+    var options = nodeListOptions();
+    lint.validateFileList(files.toArray(), options, {});
 
 });
-
